b2366d510f81d1f9ced003ddd64a6c5ed37c0def
Student <student@bluc02mg3u8fh51.local>
2018-04-24 11:25:32 -0500

    changed formatting

--- texasholdem/GameScene.swift
+++ texasholdem/GameScene.swift
@@ -14,10 +14,8 @@ class GameScene: SKScene {
    
     
     override func didMove(to view: SKView) {
-        
        
     }
-    
 
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
