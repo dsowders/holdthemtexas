ac7058864fc2201df66bbdb97376669013fe3a67
yonk <yonk@psychologyanswersglobal.local>
2018-05-04 11:38:17 -0500

    got

--- texasholdem/GameScene.swift
+++ texasholdem/GameScene.swift
@@ -50,13 +50,6 @@ class Card: NSObject
 
 
 class GameScene: SKScene {
-    var background = SKSpriteNode(imageNamed: "pokertablemydudes")
-    
-    override func didMove(to view: SKView) {
-        background.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
-        addChild(background)
-    }
-        
     var arrayNumbers: [Int] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51]
     var playerOneChips : Int = 0
     var playerTwoChips : Int = 0
